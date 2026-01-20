3295465f0023ebfd768e0a352491f44f99f95950
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-01-20 10:32:33 +0100

    Added player with basic functionality

--- CardGame/CardGame/ViewModel/Entity.cs
+++ CardGame/CardGame/ViewModel/Entity.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using CardGame.Model;
+using static CardGame.Model.Card;
 
 namespace CardGame.ViewModel
 {
     public abstract class Entity : ViewModelBase
     {
-        private Random random { get; set; }
-        private string _name { get; set; }
-        private int _health { get; set; }
-        private int _shield { get; set; }
-        private Card[] _nextCard { get; set; }
-        private Card[] _cards { get; set; }
+        protected Random random { get; set; }
+        protected string _name { get; set; }
+        protected int _health { get; set; }
+        protected int _shield { get; set; }
+        protected Card _nextCard { get; set; }
+        protected Card[] _cards { get; set; }
 
         public string Name => _name;
         public string Health => $"{_health}+{_shield}";
         public Card NextCard => _nextCard;
-        public Actions NextMove => _nextCard.Actions;
+        public Actions NextMove => _nextCard.Action;
 
     }
 }
