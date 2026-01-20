d578afd4d21d53ef2e301ea99c6cc8d338be5015
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-01-20 09:13:26 +0100

    Added Entity base class

---
+++ CardGame/CardGame/ViewModel/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CardGame.ViewModel
+{
+    public abstract class Entity : ViewModelBase
+    {
+        private Random random { get; set; }
+        private string _name { get; set; }
+        private int _health { get; set; }
+        private int _shield { get; set; }
+        private Card[] _nextCard { get; set; }
+        private Card[] _cards { get; set; }
+
+        public string Name => _name;
+        public string Health => $"{_health}+{_shield}";
+        public Card NextCard => _nextCard;
+        public Actions NextMove => _nextCard.Actions;
+
+    }
+}
