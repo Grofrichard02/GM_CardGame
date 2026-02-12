31115cfd57db67dee6c86145843badf053eed3bd
Grofrichard02 <grof.richard.laszlo.21i@egressy.info>
2026-02-12 21:20:50 +0100

    „Added _maxHealth and _dead variables”

--- CardGame/CardGame/ViewModel/Entity.cs
+++ CardGame/CardGame/ViewModel/Entity.cs
@@ -9,14 +9,18 @@ namespace CardGame.ViewModel
         protected Random random { get; set; }
         protected string _name { get; set; }
         protected int _health { get; set; }
+        protected int _maxHealth { get; set; }
         protected int _shield { get; set; }
+        protected bool _dead { get; set; }
         protected Card _nextCard { get; set; }
         protected Card[] _cards { get; set; }
 
         public string Name => _name;
-        public string Health => $"{_health}+{_shield}";
+        public string Health => $"{_health}/{_maxHealth}+{_shield}";
+
+        public bool Dead => _dead;
+
         public Card NextCard => _nextCard;
         public Actions NextMove => _nextCard.Action;
-
     }
 }
