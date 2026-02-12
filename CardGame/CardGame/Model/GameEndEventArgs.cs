6344d07cac0dc415cf2202662347f48fe9bfa019
Grofrichard02 <grof.richard.laszlo.21i@egressy.info>
2026-02-12 21:29:56 +0100

    „Added GameEventArgs class”

---
+++ CardGame/CardGame/Model/GameEndEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CardGame.Model
+{
+    public class GameEndEventArgs : EventArgs
+    {
+        public bool PlayerDead { get; }
+        public bool EnemyDead { get; }
+        public bool BossDead { get; }
+
+        public GameEndEventArgs(bool playerDead, bool enemyDead, bool bossDead)
+        {
+            PlayerDead = playerDead;
+            EnemyDead = enemyDead;
+            BossDead = bossDead;
+        }
+    }
+}
