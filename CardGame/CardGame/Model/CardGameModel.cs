97e7779a9d3dc71066d2c803422e6a6db990b796
Grofrichard02 <grof.richard.laszlo.21i@egressy.info>
2026-02-12 21:31:36 +0100

    „Added event for the end of a round or for the game ending if player dies”

--- CardGame/CardGame/Model/CardGameModel.cs
+++ CardGame/CardGame/Model/CardGameModel.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static CardGame.Model.Card;
 
 namespace CardGame.Model
@@ -11,43 +7,75 @@ namespace CardGame.Model
     {
         private Player _player;
         private Minion _minion;
+
         public Player Player => _player;
         public Minion Minion => _minion;
+
         public event EventHandler? CardUseEvent;
         public event EventHandler? NextRoundEvent;
-        public CardGameModel() { 
-        _player = new Player();
-        _minion = new Minion();
+        public event EventHandler<GameEndEventArgs>? GameEndEvent;
+
+        public CardGameModel(Player player)
+        {
+            _player = player;
+            _minion = new Minion();
         }
+
         public void PlayerCardUse(object? index)
         {
             int cardIndex = 0;
-            if(index is string strIndex)
+
+            if (index is string strIndex)
             {
                 int.TryParse(strIndex, out cardIndex);
             }
-            else if(index is int intIndex){
-                  cardIndex = intIndex;
+            else if (index is int intIndex)
+            {
+                cardIndex = intIndex;
             }
-            Card card=_player.GetCard(cardIndex);
-            if (card.Action== Actions.Attack)
+
+            Card card = _player.GetCard(cardIndex);
+
+            if (card.Action == Actions.Attack)
             {
                 _minion.Damage(card.Value);
             }
+
             _player.UseCard(cardIndex);
+
             CardUseEvent?.Invoke(this, EventArgs.Empty);
 
+            if (_player.Dead || _minion.Dead)
+            {
+                GameEndEvent?.Invoke(this,
+                    new GameEndEventArgs(_player.Dead, _minion.Dead, false));
+
+                _minion = new Minion();
+            }
         }
+
         public void NextRound()
         {
             Card card = _minion.NextCard;
-            if (card.Action==Actions.Attack)
+
+            if (card.Action == Actions.Attack)
             {
                 _player.Damage(card.Value);
             }
+
             _minion.UseCard();
             _player.GenerateCurrenthand();
+
             CardUseEvent?.Invoke(this, EventArgs.Empty);
+
+            if (_player.Dead || _minion.Dead)
+            {
+                GameEndEvent?.Invoke(this,
+                    new GameEndEventArgs(_player.Dead, _minion.Dead, false));
+
+                _minion = new Minion();
+            }
+
             NextRoundEvent?.Invoke(this, EventArgs.Empty);
         }
     }
