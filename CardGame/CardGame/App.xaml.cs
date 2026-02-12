3c5a6c947196591cc4ea1125d7ba42c27836b45e
Grofrichard02 <grof.richard.laszlo.21i@egressy.info>
2026-02-12 21:40:41 +0100

    „Added new classes to App and methods to switch between combat and navigation”

--- CardGame/CardGame/App.xaml.cs
+++ CardGame/CardGame/App.xaml.cs
@@ -1,36 +1,69 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 using CardGame.Model;
 using CardGame.View;
 using CardGame.ViewModel;
 
 namespace CardGame
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         private MainWindow _window;
         private CardGameViewModel _viewModel;
         private CardGameModel _model;
         private CombatPage _combatPage;
+
+        private NavigationModel _navigationModel;
+        private NavigationViewModel _navigationViewModel;
+        private NavigationPage _navigationPage;
+        private Player _player;
+
         public App()
         {
             Startup += App_Startup;
         }
 
+        private void ChangeToCombat()
+        {
+            _viewModel.Enabled = true;
+            _player.GenerateCurrenthand();
+            _window.DataContext = _viewModel;
+            _window.Content = _combatPage;
+        }
+
+        private void ChangeToNavigation(object? sender, GameEndEventArgs e)
+        {
+            if (e.EnemyDead)
+            {
+                _navigationViewModel.HasNotChosen = true;
+                _navigationModel.GenerateNewNavigation();
+                _window.DataContext = _navigationViewModel;
+                _window.Content = _navigationPage;
+            }
+            else if (e.PlayerDead)
+            {
+                MessageBox.Show("Game Over! You have died.");
+                _window.Close();
+            }
+        }
+
         private void App_Startup(object sender, StartupEventArgs e)
         {
             _window = new MainWindow();
-            _model = new CardGameModel();
+            _player = new Player();
+            _model = new CardGameModel(_player);
             _viewModel = new CardGameViewModel(_model);
-            _combatPage= new CombatPage();
+            _combatPage = new CombatPage();
+
+            _navigationModel = new NavigationModel(_player);
+            _navigationViewModel = new NavigationViewModel(_navigationModel);
+            _navigationPage = new NavigationPage();
+
+            _model.GameEndEvent += ChangeToNavigation;
+            _navigationViewModel.ExitNavigationEvent += (s, args) => ChangeToCombat();
+
             _window.DataContext = _viewModel;
             _window.Content = _combatPage;
             _window.Show();
         }
     }
-
 }
