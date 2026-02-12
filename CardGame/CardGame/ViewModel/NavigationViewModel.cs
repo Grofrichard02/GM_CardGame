66e976d2ae1bdef5228a6a815c41b4b67e840a4b
Grofrichard02 <grof.richard.laszlo.21i@egressy.info>
2026-02-12 21:35:24 +0100

    „Added NavigationViewModel class”

---
+++ CardGame/CardGame/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CardGame.Model;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CardGame.ViewModel
+{
+    public class NavigationViewModel : ViewModelBase
+    {
+        private bool _hasNotChosen;
+        private NavigationModel _model;
+
+        public string Name1 => _model.Choice1.Name;
+        public string Name2 => _model.Choice2.Name;
+
+        public string Description1 => _model.Choice1.Description;
+        public string Description2 => _model.Choice2.Description;
+
+        public Player Player => _model.Player;
+
+        public bool HasNotChosen
+        {
+            get => _hasNotChosen;
+            set
+            {
+                _hasNotChosen = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand<object> ChooseCommand { get; set; }
+        public RelayCommand ExitNavigationCommand { get; set; }
+
+        public event EventHandler? ExitNavigationEvent;
+
+        public void OnExitNavigation()
+        {
+            ExitNavigationEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public NavigationViewModel(NavigationModel model)
+        {
+            _model = model;
+            HasNotChosen = true;
+
+            ChooseCommand = new RelayCommand<object>(param =>
+            {
+                _model.PickChoice(int.Parse(param.ToString()));
+                HasNotChosen = false;
+            });
+
+            ExitNavigationCommand = new RelayCommand(OnExitNavigation);
+        }
+    }
+}
