73c6a0bfed34fe3cc4456fcc4aea189f673b5d90
Molnar-Mark <molnar.mark.krisztian.21i@egressy.info>
2026-02-14 22:44:05 +0100

    Added Damage, PickNextCard and UseCard methods to the Entity class

--- CardGame/CardGame/ViewModel/Entity.cs
+++ CardGame/CardGame/ViewModel/Entity.cs
@@ -16,11 +16,89 @@ namespace CardGame.ViewModel
         protected Card[] _cards { get; set; }
 
         public string Name => _name;
+
         public string Health => $"{_health}/{_maxHealth}+{_shield}";
+
+        public int CurrentHealth => _health;
+        public int MaxHealth => _maxHealth;  
+        public int Shield => _shield;
 
         public bool Dead => _dead;
 
         public Card NextCard => _nextCard;
-        public Actions NextMove => _nextCard.Action;
+
+        public string NextMove
+        {
+            get
+            {
+                if (_nextCard.Action == Actions.Empty)
+                {
+                    return string.Empty;
+                }
+
+                return $"{_nextCard.Action} ({_nextCard.Value})";
+
+            }
+        }
+
+        public virtual void Damage(int amount)
+        {
+            int remainingDamage = amount;
+
+            if (_shield > 0)
+            {
+                int shieldDamage = Math.Min(_shield, remainingDamage);
+                _shield -= shieldDamage;
+                remainingDamage -= shieldDamage;
+                OnPropertyChanged(nameof(Shield));
+                OnPropertyChanged(nameof(Health));
+            }
+
+            if (remainingDamage > 0)
+            {
+                _health -= remainingDamage;
+
+                if (_health <= 0)
+                {
+                    _health = 0;
+                    _dead = true;
+                }
+
+                OnPropertyChanged(nameof(CurrentHealth));
+                OnPropertyChanged(nameof(Health));
+                OnPropertyChanged(nameof(Dead));
+            }
+        }
+
+        protected virtual void PickNextCard()
+        {
+            if (_cards == null || _cards.Length == 0)
+            {
+                _nextCard = new Card("Empty", Actions.Empty, 0); 
+                return;
+            }
+
+            int index = random.Next(_cards.Length);
+            _nextCard = _cards[index];
+            OnPropertyChanged(nameof(NextCard));
+            OnPropertyChanged(nameof(NextMove));
+        }
+
+        public virtual void UseCard()
+        {
+            if (_nextCard.Action == Actions.Empty)
+            {
+                return;
+            }
+
+            PickNextCard();
+        }
+
+        public Entity()
+        {
+            random = new Random();
+            _shield = 0;
+            _dead = false;
+        }
     }
 }
